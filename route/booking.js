ca4a91ea8bbe07b8dd06c3858d3496a6aa5e4461
toewai <toewailyun6@gmail.com>
2025-03-09 17:07:27 +0000

    Fronted And Backend

--- route/booking.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controllerBooking = require('../controller/booking')
-
-router.get('/',controllerBooking.get);
-router.post('/',controllerBooking.postBooking);
-
-module.exports = router;
